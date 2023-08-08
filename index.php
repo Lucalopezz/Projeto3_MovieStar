b95e77d81620b9cfce8b83e9b86bd938615f5360
Lucas <lucaslops12@gmail.com>
2023-08-07 22:47:33 -0300

    novos itens

--- index.php
+++ index.php
@@ -1,98 +1,11 @@
 <?php 
-    require_once("globals.php");
-    require_once("db.php");
-
-   
+    require_once("templates/header.php");
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MovieStar</title>
-    <link rel="shortcut icon" href="<?=$BASE_URL?>img/moviestar.ico" type="image/x-icon">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.css" integrity="sha512-azoUtNAvw/SpLTPr7Z7M+5BPWGOxXOqn3/pMnCxyDqOiQd4wLVEp0+AqV8HcoUaH02Lt+9/vyDxwxHojJOsYNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!--Font-Awnsome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!--CSS -->
-    <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">
-</head>
-<body>
-    <header>
-        <nav id="main-navbar" class="navbar navbar-expand-lg">
-            <a href="<?=$BASE_URL?>" class="navbar-brand">
-                <img src="<?=$BASE_URL?>img/logo.svg" alt="MovieStar Logo" id="logo">
-                <span id="moviestar-title">MovieStar</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-
-            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <div class="input-group"> <!-- Adicionando um elemento de grupo para melhor alinhamento -->
-                    <input type="text" name="q" id="search" class="form-control" placeholder="Buscar filmes" aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
-
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $BASE_URL ?>auth.php">Entrar / Cadastrar</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>  
-    </header>
     <div id="main-container" class="container-fluid">
         <h1>Corpo do site</h1>
     </div>
 
 
-    <footer id="footer">
-        <div id="social-container">
-            <ul>
-                <li>
-                    <a href="#"><i class="fab fa-facebook-square"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fab fa-youtube"></i></a>
-                </li>
-            </ul>
-        </div>
-        <div id="footer-links-container">
-            <ul>
-                <li>
-                    <a href="#">Adiconar Filme</a>
-                </li>
-                <li>
-                    <a href="#">Adiconar Crítica</a>
-                </li>
-                <li>
-                    <a href="#">Entra / Registrar</a>
-                </li>
-            </ul>
-        </div>
-        <p>&copy; 2023 Lucas Dalossa Lopes</p>
-    </footer>
-
-
-
-
-
-
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.js" integrity="sha512-hidrIwTPV6f4zhtlHGC2IelVMrCqiHkHC9AuFJk22S95fMp5qnbhz2uLw5WH+lVWEWKg5nbRPkBsa4nNPGubsw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+<?php 
+    require_once("templates/footer.php");
+?>
---
+++ templates/footer.php
@@ -0,0 +1,40 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li>
+                    <a href="#">Adiconar Filme</a>
+                </li>
+                <li>
+                    <a href="#">Adiconar Crítica</a>
+                </li>
+                <li>
+                    <a href="#">Entra / Registrar</a>
+                </li>
+            </ul>
+        </div>
+        <p>&copy; 2023 Lucas Dalossa Lopes</p>
+    </footer>
+
+
+
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/js/bootstrap.js" integrity="sha512-hidrIwTPV6f4zhtlHGC2IelVMrCqiHkHC9AuFJk22S95fMp5qnbhz2uLw5WH+lVWEWKg5nbRPkBsa4nNPGubsw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
